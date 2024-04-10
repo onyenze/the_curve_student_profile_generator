07291eb2acce66d0d4461fad53a37576ffecab9b
onyenze <chibuezeonyenze123@gmail.com>
2024-04-10 11:26:27 +0100

    db change

--- config/configDB.js
+++ config/configDB.js
@@ -5,8 +5,8 @@ const DBpassword = process.env.DB_PASSWORD
 const DBusername = process.env.DB_USERNAME
 
 
-const db = `mongodb+srv://${DBusername}:${DBpassword}@cluster0.rbdob6c.mongodb.net/`
-
+const db = `mongodb+srv://${DBusername}:${DBpassword}@cluster0.foaaql5.mongodb.net/`
+// mongodb+srv://chibuezeonyenze123:i5qVNLFeDlSclNlO@cluster0.foaaql5.mongodb.net/
 mongoose.connect(db).then(()=>{
     console.log('Database Connected')
 }).catch((error)=>{
